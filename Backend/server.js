91a83303afa5056aa8d7e10eaafaaad145813999
Paban-blip <rpaban89@gmail.com>
2026-05-19 20:57:12 -1200

    Secound setup

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,11 @@ import app from "../Backend/app.js"
 const PORT = process.env.PORT || 3000
 console.log(PORT);
 
+// Dotenv configuration
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env' })
+dotenv.config({ path: '.env.local' })
+
 
 // Start the server
 app.listen(PORT, () => {
