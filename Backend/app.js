d4084245f459dd31dc0392c845479a568d1f48a7
Paban-blip <rpaban89@gmail.com>
2026-05-19 21:44:34 -1200

    setup cors

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+
+
+app.use(
+    cors({
+        origin: true,
+        credentials: true
+    })
+)
 
 app.get('/', (req, res) => {
     res.send('Working')
