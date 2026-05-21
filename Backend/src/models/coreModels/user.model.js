363f4f303cde98eb2b004d7a3dc7b67c819f211a
Paban-blip <rpaban89@gmail.com>
2026-05-20 13:57:53 -1200

    added user model with Schema

---
+++ Backend/src/models/coreModels/user.model.js
@@ -0,0 +1,43 @@
+// Import mongoose
+import mongoose, { mongo } from "mongoose"
+const Schema = mongoose.Schema
+
+// Create a user schema
+const userShcema = new Schema({
+    // Soft Delete/Remove
+    removed: {
+        type: Boolean,
+        default: false
+    },
+    // Account Status
+    enabled: {
+        type: Boolean,
+        default: false,
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        trim: true,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        minlenght: [3, 'Name must be 3 characters Long']
+    }, surname: {
+        type: String,
+        minlenght: [3, 'Surname must be 3 characters Long']
+    },
+    photo: {
+        type: String, // URL from Cloudinary
+        trim: true
+    },
+    role: {
+        type: String,
+        default: 'employee',
+        enum: ['owner', 'admin', 'manager', 'employee']
+    },
+},
+    { timestamps: true })
+
+// Export user Schema
