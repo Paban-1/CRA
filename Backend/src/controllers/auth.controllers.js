be11171ca318c5cb0c9d25da357b42792020c934
Paban-blip <rpaban89@gmail.com>
2026-05-23 09:58:54 -1200

    create auth controller :Register

---
+++ Backend/src/controllers/auth.controllers.js
@@ -0,0 +1,47 @@
+// Import models
+import User from '../models/coreModels/user.model.js'
+import UserAuth from '../models/coreModels/userAuth.model.js'
+
+// Regiseter a new user
+export const register = async (req, res) => {
+    // try chatch block to handle errors
+    try {
+        const { email, name, surname, password } = req.body
+
+        // check for exitsing user with the same email
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({ message: 'User with this email already exists' })
+        }
+
+        // Create new user
+        const createdUser = await User.create({
+            email,
+            name,
+            surname,
+        })
+
+        // hash password
+        const userAuthDoc = new UserAuth()
+        const hashedPassword = await userAuthDoc.generateHash(password)
+
+        // generate eamil verification token
+        const emailToken = crypto.randomBytes(32).toString('hex')
+        emailTokenExpiry = Date.now() + 24 * 60 * 60 * 1000 // 24 hours
+
+        await UserAuth.create({
+            user: createdUser._id,
+            password: hashedPassword,
+            emailToken,
+            emailTokenExpiry
+        });
+
+        // send verification email Added soon
+        return res.status(201).json({
+            success: true,
+            message: 'Registration successful! Please check your email to verify your account'
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
